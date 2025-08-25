6018734dfb9a303164250860367ec1d1d7a78a3a
Kasa28 <agliullinaksenija@gmail.com>
2025-08-25 20:34:06 +0200

    img.jpg

--- scripts/db.js
+++ scripts/db.js
@@ -5,28 +5,28 @@ const myDishes = {
       name: "Gebratener Reis mit Gemüse",
       description: "Mit frischem Gemüse, Sojasauce und Eiern im Wok gebraten",
       price: 9.99,
-      image: "./assets/img/rmg.png"
+      image: "./assets/img/rmg.jpg"
     },
     {
       id: 2,
       name: "Gebratener Reis mit Huhn",
       description: "Reis mit zarten Hühnerstreifen, Gemüse und Sojasauce",
       price: 10.99,
-      image: "./assets/img/rmh.png"
+      image: "./assets/img/rmh.jpg"
     },
     {
       id: 4,
       name: "Gebratener Reis mit Rind",
       description: "Würziger Reis mit mariniertem Rindfleisch und Paprika",
       price: 11.49,
-      image: "./assets/img/rind.png"
+      image: "./assets/img/rind.jpg"
     },
     {
       id: 10,
       name: "Gebratener Reis mit Ente",
       description: "Knusprige Ente auf gebratenem Reis mit Gemüse",
       price: 12.99,
-      image: "./assets/img/ente.png"
+      image: "./assets/img/ente.jpg"
     }
   ],
   "Noodles": [
@@ -35,28 +35,28 @@ const myDishes = {
       name: "Gebratene Nudeln mit Gemüse",
       description: "Nudeln mit Gemüse und Sojasauce gebraten",
       price: 9.49,
-      image: "./assets/img/gemüse.png"
+      image: "./assets/img/gemüse.jpg"
     },
     {
       id: 5,
       name: "Udon-Nudeln mit Garnelen",
       description: "Japanische Udon-Nudeln mit knackigem Gemüse und Garnelen",
       price: 12.49,
-      image: "./assets/img/reis mit garnelen.png"
+      image: "./assets/img/reis mit garnelen.jpg"
     },
     {
       id: 6,
       name: "Ramen mit Hühnerbrust",
       description: "Ramen-Nudeln in Brühe mit Hühnerbrust und Ei",
       price: 11.99,
-      image: "./assets/img/ramen.png"
+      image: "./assets/img/rmh.jpg"
     },
     {
       id: 11,
       name: "Yaki Soba mit Rind",
       description: "Gebratene Soba-Nudeln mit Rindfleisch und Sesam",
       price: 11.49,
-      image: "./assets/img/yakisoba.png"
+      image: "./assets/img/yakisoba.jpg"
     }
   ],
   "Soups": [
@@ -65,28 +65,28 @@ const myDishes = {
       name: "Miso Suppe",
       description: "Traditionelle japanische Misosuppe mit Tofu und Algen",
       price: 4.99,
-      image: "./assets/img/miso.png"
+      image: "./assets/img/miso.jpg"
     },
     {
       id: 8,
       name: "Sauer-Scharf-Suppe",
       description: "Würzige Suppe mit Bambus, Pilzen und Tofu",
       price: 5.49,
-      image: "./assets/img/sauer.png"
+      image: "./assets/img/sauer.jpg"
     },
     {
       id: 9,
       name: "Wantan-Suppe",
       description: "Klare Brühe mit gefüllten Teigtaschen und Gemüse",
       price: 6.49,
-      image: "./assets/img/wantam.png"
+      image: "./assets/img/wantam.jpg"
     },
     {
       id: 12,
       name: "Kokossuppe mit Huhn",
       description: "Thailändische Tom Kha Gai mit Kokosmilch und Huhn",
       price: 6.99,
-      image: "./assets/img/kokossuppe.png"
+      image: "./assets/img/kokossuppe.jpg"
     }
   ],
   "Desserts": [
@@ -95,21 +95,21 @@ const myDishes = {
       name: "Sesambällchen",
       description: "Frittierte Reisbällchen mit süßer Bohnenfüllung",
       price: 4.99,
-      image: "./assets/img/sesam.png"
+      image: "./assets/img/sesam.jpg"
     },
     {
       id: 14,
       name: "Mango Sticky Rice",
       description: "Klebreis mit Kokosmilch und frischer Mango",
       price: 5.49,
-      image: "./assets/img/mango.png"
+      image: "./assets/img/mango.jpg"
     },
     {
       id: 15,
       name: "Gebackene Banane mit Honig",
       description: "Knusprige Banane mit Honig und Sesam",
       price: 4.49,
-      image: "./assets/img/bananen.png"
+      image: "./assets/img/bananen.jpg"
     }
   ],
   "Drinks": [
@@ -118,21 +118,21 @@ const myDishes = {
       name: "Hausgemachter Eistee",
       description: "Frisch gebrühter Schwarztee mit Zitrone",
       price: 2.99,
-      image: "./assets/img/eistee.png"
+      image: "./assets/img/eistee.jpg"
     },
     {
       id: 17,
       name: "Lychee Limo",
       description: "Erfrischendes Getränk mit Lycheegeschmack",
       price: 3.49,
-      image: "./assets/img/lyche.png"
+      image: "./assets/img/lyche.jpg"
     },
     {
       id: 18,
       name: "Mango Lassi",
       description: "Joghurtgetränk mit süßer Mango",
       price: 3.99,
-      image: "./assets/img/lassi.png"
+      image: "./assets/img/lassi.jpg"
     }
   ]
 };
